8e8d0dec4f8b26d20012bf995fe94753f47a4fb2
Akonkwa Mubagwa <akonkwamubagwa@Akonkwas-MBP.lan>
2026-04-30 08:35:16 -0400

    Force dynamic app shell for seeded universe boot

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import AppShell from "../components/AppShell";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function HomePage() {
   return <AppShell />;
 }
